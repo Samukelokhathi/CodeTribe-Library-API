1e9ee643c4ab0f3a212e4579d3350e384f2a40cd
Samkelo Khathi <sbkhathi005@gmail.com>
2026-09-21 15:19:28 +0200

    Added updateBooks function and deleteBook

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -15,12 +15,7 @@ export function getBookById(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-
-export function createBook(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+export function createBook(req: Request, res: Response, next: NextFunction) {
   const { title, authorId, publishedDate } = req.body;
   const newBook: book = {
     id: books.length + 1,
@@ -30,4 +25,31 @@ export function createBook(
   };
   books.push(newBook);
   res.status(201).json(newBook);
+}
+
+export function updateBook(req: Request, res: Response, next: NextFunction) {
+  const id = Number(req.params.id);
+  const book = books.find((b) => b.id === id);
+  if (book) {
+    const { title, authorId, publishedDate } = req.body;
+
+    book.title = title ?? book.title;
+    book.authorId = authorId ?? book.authorId;
+    book.publishedDate = publishedDate ?? book.publishedDate;
+
+    res.json(book);
+  } else {
+    res.status(404).json({ error: "Book not found" });
+  }
+}
+
+export function deleteBook(req: Request, res: Response, next: NextFunction) {
+  const id = Number(req.params.id);
+  const index = books.findIndex((b) => b.id === id);
+  if (index !== -1) {
+    books.splice(index, 1);
+    res.status(200).json({ message: "Book deleted successfully" });
+  } else {
+    res.status(404).json({ error: "Book not found" });
+  }
 }
--- src/routes/bookRoute.ts
+++ src/routes/bookRoute.ts
@@ -3,8 +3,8 @@ import {
   getAllBooks,
   getBookById,
   createBook,
-  // updateBook,
-  //     deleteBook,
+  updateBook,
+  deleteBook,
 } from "../controllers/booksController.js";
 
 const bookRouter = Router();
@@ -12,7 +12,7 @@ const bookRouter = Router();
 bookRouter.get("/", getAllBooks);
 bookRouter.get("/:id", getBookById);
 bookRouter.post("/", createBook);
-// bookRouter.put("/:id", updateBook);
-// bookRouter.delete("/:id", deleteBook);
+bookRouter.put("/:id", updateBook);
+bookRouter.delete("/:id", deleteBook);
 
 export default bookRouter;
